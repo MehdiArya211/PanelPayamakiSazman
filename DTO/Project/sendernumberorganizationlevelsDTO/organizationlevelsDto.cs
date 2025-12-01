a6ccdd92725a707504b8195360c8c66cf4ce1031
Mk <Mk@DESKTOP-E4FL5T2>
2025-12-01 12:48:48 +0330

    majid

---
+++ DTO/Project/sendernumberorganizationlevelsDTO/organizationlevelsDto.cs
@@ -0,0 +1,26 @@
+﻿using DTO.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO.Project.sendernumberorganizationlevelsDTO
+{
+    class organizationlevelsDto :  EntityDTO
+    {
+        public string Code { get; set; }
+
+
+
+        [MaxLength(200, ErrorMessage = "{0} حداکثر می تواند {1} کاراکتر باشد.")]
+        public string Title { get; set; }
+
+
+
+        [MaxLength(5000, ErrorMessage = "{0} حداکثر می تواند {1} کاراکتر باشد.")]
+        public string description { get; set; }
+
+    }
+}
---
+++ Software/Areas/Project/Controllers/sendernumberorganizationlevels.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Software.Areas.Project.Controllers
+{
+    public class sendernumberorganizationlevels : Controller
+    {
+
+        public sendernumberorganizationlevels()
+        {
+                
+        }
+
+
+
+
+        #region نمایش 
+        public IActionResult Index()
+        {
+            return View();
+        }
+        #endregion
+
+
+    }
+}
