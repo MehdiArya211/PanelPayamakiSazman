d070f6480cd3206e9b35339d4ba1731f5d6a00c9
Reveshtian_dev <reveshtian9062@gmail.com>
2025-12-10 00:15:46 +0330

    mohammad

---
+++ Software/Areas/Project/SenderNumberOrganizationLevel/Controllers/SenderNumberOrganizationLevelController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Software.Areas.Project.SenderNumberOrganizationLevel.Controllers
+{
+    [Area("Project")]
+    public class SenderNumberOrganizationLevelController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
